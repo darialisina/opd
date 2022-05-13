193ba5875a1dad557860d45b7211a4830991e31d
dasha <dasha.lisina35@gmail.com>
2022-05-13 19:29:14 +0300

    add tests file for task2

---
+++ test_task1/UnitTests_task2.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace lab1_task2
+{
+    public class UnitTests_task2
+    {
+        [Test]
+        public void Subscriber1Login()
+        {
+            Subscriber1 subscriber1 = new Subscriber1("Dasha");
+            string login = subscriber1.Login;
+
+            string res = "Dasha";
+
+            Assert.AreEqual(res, login);
+        } 
+    }
+}
