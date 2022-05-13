08d9ebd7e5ca59209b48e8051c628deb86aa080d
dasha <dasha.lisina35@gmail.com>
2022-05-13 14:52:45 +0300

    add countdown

---
+++ lab1_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace lab1_task2
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Subscriber1 sub1 = new Subscriber1("Ivan");
+            Subscriber2 sub2 = new Subscriber2("Vasya");
+            Subscriber3 sub3 = new Subscriber3("Ura");
+
+            Countdown countdown = new Countdown();
+
+            countdown.subscribe(sub1);
+            countdown.subscribe(sub2);
+            countdown.subscribe(sub3);
+
+            countdown.notifySubscribers(1000);
+        }
+    }
+}
