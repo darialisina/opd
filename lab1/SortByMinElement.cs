c4d654caa2c74141dab6c2c0ef196a33ffad1858
dasha <dasha.lisina35@gmail.com>
2022-05-13 14:49:25 +0300

    made sort

---
+++ lab1/SortByMinElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class SortByMinElement:IBaseAlgorithm
+    {
+        public int Handle(int[] row)
+        {
+            return row.Min();
+        }
+    }
+}
---
+++ lab1/SortMatrix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class SortMatrix
+    {
+        public void sortMatrix(Matrix matrix, bool sign,  IBaseAlgorithm algorithm)
+        {
+            for (int i = 1; i < matrix.Rows; i++)
+            {
+                int key = algorithm.Handle(matrix[i]);
+                int[] tempRow = matrix[i]; 
+                int j = i - 1;
+
+                while (j >= 0 && direction(sign, algorithm.Handle(matrix[j]), key))
+                {
+                    matrix[j + 1] = matrix[j];
+                    j = j - 1;
+                }
+                matrix[j + 1] = tempRow;
+            }
+
+        }
+
+        private bool direction(bool sign, int a1, int a2)
+        {
+            if (sign)
+            {
+                return a1 > a2;
+            }
+            else
+            {
+                return a1 < a2;
+            }
+        }
+    }
+}
